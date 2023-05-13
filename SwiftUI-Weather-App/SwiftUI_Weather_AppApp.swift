1ee3bbc3a2adc644ba8471f97ca837c96cb119dd
Mohamed ELkilany <mohamedsayedelkilany@gmail.com>
2023-05-13 16:09:34 +0300

    Initial Commit

---
+++ SwiftUI-Weather-App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI-Weather-App
+//
+//  Created by Elkilany on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
